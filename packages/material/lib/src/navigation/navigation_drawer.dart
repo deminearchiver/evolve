387d657992627a75e2b47a3468ed288ad1d64679
DeMineArchiver <minearchiver@gmail.com>
2025-03-29 13:36:50 +0300

    feat(material): experimental NavigationDrawer and NavigationDrawerDestination implementation

---
+++ packages/material/lib/src/navigation/navigation_drawer.dart
@@ -0,0 +1,258 @@
+import 'package:material/material.dart'
+    hide
+        NavigationDrawer,
+        NavigationDrawerDestination,
+        NavigationDrawerTheme,
+        NavigationDrawerThemeData;
+import 'package:flutter/material.dart' as flutter_material;
+
+import 'navigation_drawer_theme.dart';
+
+// TODO: remove these globals used during development
+late final flutter_material.NavigationDrawer _navigationDrawer;
+late final flutter_material.NavigationDrawerThemeData
+_navigationDrawerThemeData;
+
+class NavigationDrawer extends StatelessWidget {
+  const NavigationDrawer({
+    super.key,
+    this.backgroundColor,
+    this.shadowColor,
+    this.elevation,
+    required this.child,
+  });
+
+  final Color? backgroundColor;
+  final Color? shadowColor;
+  final double? elevation;
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final navigationDrawerTheme = NavigationDrawerTheme.of(context);
+    return Drawer(
+      backgroundColor: backgroundColor ?? navigationDrawerTheme.backgroundColor,
+      shadowColor: shadowColor ?? navigationDrawerTheme.shadowColor,
+      elevation: elevation ?? navigationDrawerTheme.elevation,
+      surfaceTintColor: Colors.transparent,
+      clipBehavior: Clip.antiAlias,
+      child: SafeArea(bottom: false, child: child),
+    );
+  }
+}
+
+class NavigationDrawerDestination extends StatefulWidget {
+  const NavigationDrawerDestination({
+    super.key,
+    this.onTap,
+    this.selected = false,
+    this.activeIndicatorColor,
+    this.activeIndicatorSize,
+    this.activeIndicatorShape,
+    required this.icon,
+    this.selectedIcon,
+    required this.label,
+  });
+
+  final VoidCallback? onTap;
+  final bool selected;
+
+  final Color? activeIndicatorColor;
+  final Size? activeIndicatorSize;
+  final ShapeBorder? activeIndicatorShape;
+
+  final Widget icon;
+  final Widget? selectedIcon;
+  final Widget label;
+
+  @override
+  State<NavigationDrawerDestination> createState() =>
+      _NavigationDrawerDestinationState();
+}
+
+class _NavigationDrawerDestinationState
+    extends State<NavigationDrawerDestination> {
+  Widget _buildSelectable(BuildContext context, Animation<double> animation) {
+    const Set<WidgetState> disabledState = <WidgetState>{WidgetState.disabled};
+    const Set<WidgetState> selectedState = <WidgetState>{WidgetState.selected};
+    const Set<WidgetState> unselectedState = <WidgetState>{};
+
+    final NavigationDrawerThemeData navigationDrawerTheme =
+        NavigationDrawerTheme.of(context);
+
+    const bool enabled = true;
+
+    return _NavigationDestinationBuilder(
+      animation: animation,
+      color: widget.activeIndicatorColor,
+      size: widget.activeIndicatorSize,
+      shape: widget.activeIndicatorShape,
+      buildIcon: (BuildContext context) {
+        final Widget selectedIconWidget = IconTheme.merge(
+          data: navigationDrawerTheme.iconTheme.resolve(
+            enabled ? selectedState : disabledState,
+          ),
+          child: widget.selectedIcon ?? widget.icon,
+        );
+        final Widget unselectedIconWidget = IconTheme.merge(
+          data: navigationDrawerTheme.iconTheme.resolve(
+            enabled ? unselectedState : disabledState,
+          ),
+          child: widget.icon,
+        );
+
+        return animation.isForwardOrCompleted
+            ? selectedIconWidget
+            : unselectedIconWidget;
+      },
+      buildLabel: (BuildContext context) {
+        final effectiveSelectedLabelTextStyle = navigationDrawerTheme
+            .labelTextStyle
+            .resolve(enabled ? selectedState : disabledState);
+        final effectiveUnselectedLabelTextStyle = navigationDrawerTheme
+            .labelTextStyle
+            .resolve(enabled ? unselectedState : disabledState);
+
+        return DefaultTextStyle.merge(
+          style:
+              animation.isForwardOrCompleted
+                  ? effectiveSelectedLabelTextStyle
+                  : effectiveUnselectedLabelTextStyle,
+          child: widget.label,
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final motionTheme = MotionTheme.of(context);
+    return SelectableAnimatedBuilder(
+      selected: widget.selected,
+      duration: motionTheme.duration.long2,
+      builder: _buildSelectable,
+    );
+  }
+}
+
+class _NavigationDestinationBuilder extends StatelessWidget {
+  const _NavigationDestinationBuilder({
+    this.onTap,
+    required this.animation,
+    this.color,
+    this.size,
+    this.shape,
+    required this.buildIcon,
+    required this.buildLabel,
+  });
+
+  final VoidCallback? onTap;
+  final Animation<double> animation;
+
+  final WidgetBuilder buildIcon;
+
+  final WidgetBuilder buildLabel;
+
+  final Color? color;
+  final Size? size;
+  final ShapeBorder? shape;
+
+  @override
+  Widget build(BuildContext context) {
+    final navigationDrawerTheme = NavigationDrawerTheme.of(context);
+
+    final resolvedColor = color ?? navigationDrawerTheme.activeIndicatorColor;
+    final resolvedSize = size ?? navigationDrawerTheme.activeIndicatorSize;
+    final resolvedShape = shape ?? navigationDrawerTheme.activeIndicatorShape;
+
+    final Widget inkWell = InkWell(
+      highlightColor: Colors.transparent,
+      onTap: onTap,
+      customBorder: resolvedShape,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          NavigationIndicator(
+            animation: animation,
+            color: resolvedColor,
+            shape: resolvedShape,
+            width: resolvedSize.width,
+            height: resolvedSize.height,
+          ),
+          Flex.horizontal(
+            children: <Widget>[
+              const SizedBox(width: 16),
+              buildIcon(context),
+              const SizedBox(width: 12),
+              buildLabel(context),
+            ],
+          ),
+        ],
+      ),
+    );
+
+    final Widget destination = Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: _NavigationDestinationSemantics(
+        animation: animation,
+        // TODO: fix semantics
+        index: 0,
+        totalNumberOfDestinations: 1,
+        child: SizedBox(
+          height: navigationDrawerTheme.activeIndicatorSize.height,
+          child: inkWell,
+        ),
+      ),
+    );
+
+    return destination;
+  }
+}
+
+class _NavigationDestinationSemantics extends StatelessWidget {
+  const _NavigationDestinationSemantics({
+    required this.animation,
+    required this.index,
+    required this.totalNumberOfDestinations,
+    required this.child,
+  });
+
+  final Animation<double> animation;
+
+  final int index;
+  final int totalNumberOfDestinations;
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final MaterialLocalizations localizations = MaterialLocalizations.of(
+      context,
+    );
+    // The AnimationStatusBuilder will make sure that the semantics update to
+    // "selected" when the animation status changes.
+    return StatusTransitionBuilder(
+      animation: animation,
+      builder: (context, child) {
+        return Semantics(
+          selected: animation.isForwardOrCompleted,
+          container: true,
+          child: child,
+        );
+      },
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          child,
+          Semantics(
+            label: localizations.tabLabel(
+              tabIndex: index + 1,
+              tabCount: totalNumberOfDestinations,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ packages/material/lib/src/navigation/navigation_drawer_theme.dart
@@ -0,0 +1,526 @@
+import 'package:flutter/foundation.dart';
+import 'package:material/material.dart';
+
+@immutable
+class NavigationDrawerThemeDataPartial with Diagnosticable {
+  const NavigationDrawerThemeDataPartial({
+    this.backgroundColor,
+    this.shadowColor,
+    this.elevation,
+    this.activeIndicatorColor,
+    this.activeIndicatorSize,
+    this.activeIndicatorShape,
+    this.iconTheme,
+    this.labelTextStyle,
+  });
+
+  final Color? backgroundColor;
+
+  final Color? shadowColor;
+
+  final double? elevation;
+
+  final Color? activeIndicatorColor;
+
+  final Size? activeIndicatorSize;
+
+  final ShapeBorder? activeIndicatorShape;
+
+  final WidgetStateProperty<IconThemeDataPartial>? iconTheme;
+
+  final WidgetStateProperty<TextStyle>? labelTextStyle;
+
+  NavigationDrawerThemeDataPartial copyWith({
+    Color? backgroundColor,
+    Color? shadowColor,
+    double? elevation,
+    Color? activeIndicatorColor,
+    Size? activeIndicatorSize,
+    ShapeBorder? activeIndicatorShape,
+    WidgetStateProperty<IconThemeDataPartial>? iconTheme,
+    WidgetStateProperty<TextStyle>? labelTextStyle,
+  }) {
+    if (backgroundColor == null &&
+        shadowColor == null &&
+        elevation == null &&
+        activeIndicatorColor == null &&
+        activeIndicatorSize == null &&
+        activeIndicatorShape == null &&
+        iconTheme == null &&
+        labelTextStyle == null) {
+      return this;
+    }
+    return NavigationDrawerThemeDataPartial(
+      backgroundColor: backgroundColor ?? this.backgroundColor,
+      shadowColor: shadowColor ?? this.shadowColor,
+      elevation: elevation ?? this.elevation,
+      activeIndicatorColor: activeIndicatorColor ?? this.activeIndicatorColor,
+      activeIndicatorSize: activeIndicatorSize ?? this.activeIndicatorSize,
+      activeIndicatorShape: activeIndicatorShape ?? this.activeIndicatorShape,
+      iconTheme: iconTheme ?? this.iconTheme,
+      labelTextStyle: labelTextStyle ?? this.labelTextStyle,
+    );
+  }
+
+  NavigationDrawerThemeDataPartial merge(
+    NavigationDrawerThemeDataPartial? other,
+  ) {
+    if (other == null) return this;
+    return copyWith(
+      backgroundColor: other.backgroundColor,
+      shadowColor: other.shadowColor,
+      elevation: other.elevation,
+      activeIndicatorColor: other.activeIndicatorColor,
+      activeIndicatorSize: other.activeIndicatorSize,
+      activeIndicatorShape: other.activeIndicatorShape,
+      iconTheme: other.iconTheme,
+      labelTextStyle: other.labelTextStyle,
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(
+      ColorProperty("backgroundColor", backgroundColor, defaultValue: null),
+    );
+    properties.add(
+      ColorProperty("shadowColor", shadowColor, defaultValue: null),
+    );
+    properties.add(DoubleProperty("elevation", elevation, defaultValue: null));
+    properties.add(
+      ColorProperty(
+        "activeIndicatorColor",
+        activeIndicatorColor,
+        defaultValue: null,
+      ),
+    );
+    properties.add(
+      DiagnosticsProperty<Size>(
+        "activeIndicatorSize",
+        activeIndicatorSize,
+        defaultValue: null,
+      ),
+    );
+    properties.add(
+      DiagnosticsProperty<ShapeBorder>(
+        "activeIndicatorShape",
+        activeIndicatorShape,
+        defaultValue: null,
+      ),
+    );
+    properties.add(
+      DiagnosticsProperty<WidgetStateProperty<IconThemeDataPartial>>(
+        "iconTheme",
+        iconTheme,
+        defaultValue: null,
+      ),
+    );
+    properties.add(
+      DiagnosticsProperty<WidgetStateProperty<TextStyle>>(
+        "labelTextStyle",
+        labelTextStyle,
+        defaultValue: null,
+      ),
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        other is NavigationDrawerThemeDataPartial &&
+            backgroundColor == other.backgroundColor &&
+            shadowColor == other.shadowColor &&
+            elevation == other.elevation &&
+            activeIndicatorColor == other.activeIndicatorColor &&
+            activeIndicatorSize == other.activeIndicatorSize &&
+            activeIndicatorShape == other.activeIndicatorShape &&
+            iconTheme == other.iconTheme &&
+            labelTextStyle == other.labelTextStyle;
+  }
+
+  @override
+  int get hashCode => Object.hash(
+    backgroundColor,
+    shadowColor,
+    elevation,
+    activeIndicatorColor,
+    activeIndicatorSize,
+    activeIndicatorShape,
+    iconTheme,
+    labelTextStyle,
+  );
+}
+
+@immutable
+class NavigationDrawerThemeData
+    with Diagnosticable
+    implements NavigationDrawerThemeDataPartial {
+  const NavigationDrawerThemeData({
+    required this.backgroundColor,
+    required this.shadowColor,
+    required this.elevation,
+    required this.activeIndicatorColor,
+    required this.activeIndicatorSize,
+    required this.activeIndicatorShape,
+    required this.iconTheme,
+    required this.labelTextStyle,
+  });
+
+  // TODO: decide if we should include a factory here
+  // factory NavigationDrawerThemeData.fallback({required BuildContext context}) =
+  //     _NavigationDrawerThemeDataFallback;
+
+  @override
+  final Color backgroundColor;
+
+  @override
+  final Color shadowColor;
+
+  @override
+  final double elevation;
+
+  @override
+  final Color activeIndicatorColor;
+
+  @override
+  final Size activeIndicatorSize;
+
+  @override
+  final ShapeBorder activeIndicatorShape;
+
+  @override
+  final WidgetStateProperty<IconThemeDataPartial> iconTheme;
+
+  @override
+  final WidgetStateProperty<TextStyle> labelTextStyle;
+
+  @override
+  NavigationDrawerThemeData copyWith({
+    Color? backgroundColor,
+    Color? shadowColor,
+    double? elevation,
+    Color? activeIndicatorColor,
+    Size? activeIndicatorSize,
+    ShapeBorder? activeIndicatorShape,
+    WidgetStateProperty<IconThemeDataPartial>? iconTheme,
+    WidgetStateProperty<TextStyle>? labelTextStyle,
+  }) {
+    if (backgroundColor == null &&
+        shadowColor == null &&
+        elevation == null &&
+        activeIndicatorColor == null &&
+        activeIndicatorSize == null &&
+        activeIndicatorShape == null &&
+        iconTheme == null &&
+        labelTextStyle == null) {
+      return this;
+    }
+    return NavigationDrawerThemeData(
+      backgroundColor: backgroundColor ?? this.backgroundColor,
+      shadowColor: shadowColor ?? this.shadowColor,
+      elevation: elevation ?? this.elevation,
+      activeIndicatorColor: activeIndicatorColor ?? this.activeIndicatorColor,
+      activeIndicatorSize: activeIndicatorSize ?? this.activeIndicatorSize,
+      activeIndicatorShape: activeIndicatorShape ?? this.activeIndicatorShape,
+      iconTheme: iconTheme ?? this.iconTheme,
+      labelTextStyle: labelTextStyle ?? this.labelTextStyle,
+    );
+  }
+
+  @override
+  NavigationDrawerThemeData merge(NavigationDrawerThemeDataPartial? other) {
+    if (other == null) return this;
+    return copyWith(
+      backgroundColor: other.backgroundColor,
+      shadowColor: other.shadowColor,
+      elevation: other.elevation,
+      activeIndicatorColor: other.activeIndicatorColor,
+      activeIndicatorSize: other.activeIndicatorSize,
+      activeIndicatorShape: other.activeIndicatorShape,
+      iconTheme: other.iconTheme,
+      labelTextStyle: other.labelTextStyle,
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(ColorProperty("backgroundColor", backgroundColor));
+    properties.add(ColorProperty("shadowColor", shadowColor));
+    properties.add(DoubleProperty("elevation", elevation));
+    properties.add(ColorProperty("activeIndicatorColor", activeIndicatorColor));
+    properties.add(
+      DiagnosticsProperty<Size>("activeIndicatorSize", activeIndicatorSize),
+    );
+    properties.add(
+      DiagnosticsProperty<ShapeBorder>(
+        "activeIndicatorShape",
+        activeIndicatorShape,
+      ),
+    );
+    properties.add(
+      DiagnosticsProperty<WidgetStateProperty<IconThemeDataPartial>>(
+        "iconTheme",
+        iconTheme,
+      ),
+    );
+    properties.add(
+      DiagnosticsProperty<WidgetStateProperty<TextStyle>>(
+        "labelTextStyle",
+        labelTextStyle,
+      ),
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        other is NavigationDrawerThemeData &&
+            backgroundColor == other.backgroundColor &&
+            shadowColor == other.shadowColor &&
+            elevation == other.elevation &&
+            activeIndicatorColor == other.activeIndicatorColor &&
+            activeIndicatorSize == other.activeIndicatorSize &&
+            activeIndicatorShape == other.activeIndicatorShape &&
+            iconTheme == other.iconTheme &&
+            labelTextStyle == other.labelTextStyle;
+  }
+
+  @override
+  int get hashCode => Object.hash(
+    backgroundColor,
+    shadowColor,
+    elevation,
+    activeIndicatorColor,
+    activeIndicatorSize,
+    activeIndicatorShape,
+    iconTheme,
+    labelTextStyle,
+  );
+}
+
+class NavigationDrawerTheme extends InheritedTheme {
+  const NavigationDrawerTheme({
+    super.key,
+    required this.data,
+    required super.child,
+  });
+
+  final NavigationDrawerThemeData data;
+
+  @override
+  bool updateShouldNotify(covariant NavigationDrawerTheme oldWidget) {
+    return data != oldWidget.data;
+  }
+
+  @override
+  Widget wrap(BuildContext context, Widget child) {
+    return NavigationDrawerTheme(data: data, child: child);
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(
+      DiagnosticsProperty<NavigationDrawerThemeData>("data", data),
+    );
+  }
+
+  static Widget merge({
+    Key? key,
+    required NavigationDrawerThemeDataPartial data,
+    required Widget child,
+  }) {
+    return Builder(
+      builder: (BuildContext context) {
+        return NavigationDrawerTheme(
+          key: key,
+          data: of(context).merge(data),
+          child: child,
+        );
+      },
+    );
+  }
+
+  static NavigationDrawerThemeData? maybeOf(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<NavigationDrawerTheme>()
+        ?.data;
+  }
+
+  static NavigationDrawerThemeData of(BuildContext context) {
+    final result = maybeOf(context);
+    return result ?? _NavigationDrawerThemeDataFallback(context: context);
+  }
+}
+
+class _NavigationDrawerThemeDataFallback
+    with Diagnosticable
+    implements NavigationDrawerThemeData {
+  _NavigationDrawerThemeDataFallback({required this.context})
+    : shadowColor = Colors.transparent,
+      activeIndicatorSize = const Size(336, 56);
+
+  final BuildContext context;
+  late final ColorThemeData _color = ColorTheme.of(context);
+  late final ShapeThemeData _shape = ShapeTheme.of(context);
+  late final ElevationThemeData _elevation = ElevationTheme.of(context);
+  late final TextTheme _text = TextTheme.of(context);
+
+  @override
+  Color get backgroundColor => _color.surfaceContainerLow;
+
+  @override
+  final Color shadowColor;
+
+  @override
+  double get elevation => _elevation.level1;
+
+  @override
+  Color get activeIndicatorColor => _color.secondaryContainer;
+
+  @override
+  final Size activeIndicatorSize;
+
+  @override
+  WidgetStateProperty<IconThemeDataPartial> get iconTheme =>
+      WidgetStateProperty.resolveWith((states) {
+        final Color color;
+        final double fill;
+        if (states.contains(WidgetState.selected)) {
+          color = _color.onSecondaryContainer;
+          fill = 1.0;
+        } else {
+          color = _color.onSurfaceVariant;
+          fill = 0.0;
+        }
+        return IconThemeDataPartial(
+          fill: fill,
+          size: 24,
+          opticalSize: 24,
+          color: color,
+        );
+      });
+
+  @override
+  WidgetStateProperty<TextStyle> get labelTextStyle =>
+      WidgetStateProperty.resolveWith((states) {
+        final Color color;
+        if (states.contains(WidgetState.selected)) {
+          color = _color.onSecondaryContainer;
+        } else {
+          color = _color.onSurfaceVariant;
+        }
+        return _text.labelLarge!.copyWith(color: color);
+      });
+
+  @override
+  ShapeBorder get activeIndicatorShape => CornersBorder(
+    delegate: _shape.corner.family.delegate,
+    corners: Corners.all(_shape.corner.full),
+  );
+
+  @override
+  NavigationDrawerThemeData copyWith({
+    Color? backgroundColor,
+    Color? shadowColor,
+    double? elevation,
+    Color? activeIndicatorColor,
+    Size? activeIndicatorSize,
+    ShapeBorder? activeIndicatorShape,
+    WidgetStateProperty<IconThemeDataPartial>? iconTheme,
+    WidgetStateProperty<TextStyle>? labelTextStyle,
+  }) {
+    if (backgroundColor == null &&
+        shadowColor == null &&
+        elevation == null &&
+        activeIndicatorColor == null &&
+        activeIndicatorSize == null &&
+        activeIndicatorShape == null &&
+        labelTextStyle == null) {
+      return this;
+    }
+    return NavigationDrawerThemeData(
+      backgroundColor: backgroundColor ?? this.backgroundColor,
+      shadowColor: shadowColor ?? this.shadowColor,
+      elevation: elevation ?? this.elevation,
+      activeIndicatorColor: activeIndicatorColor ?? this.activeIndicatorColor,
+      activeIndicatorSize: activeIndicatorSize ?? this.activeIndicatorSize,
+      activeIndicatorShape: activeIndicatorShape ?? this.activeIndicatorShape,
+      iconTheme: iconTheme ?? this.iconTheme,
+      labelTextStyle: labelTextStyle ?? this.labelTextStyle,
+    );
+  }
+
+  @override
+  NavigationDrawerThemeData merge(NavigationDrawerThemeDataPartial? other) {
+    if (other == null) return this;
+    return copyWith(
+      backgroundColor: other.backgroundColor,
+      shadowColor: other.shadowColor,
+      elevation: other.elevation,
+      activeIndicatorColor: other.activeIndicatorColor,
+      activeIndicatorSize: other.activeIndicatorSize,
+      activeIndicatorShape: other.activeIndicatorShape,
+      iconTheme: other.iconTheme,
+      labelTextStyle: other.labelTextStyle,
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(ColorProperty("backgroundColor", backgroundColor));
+    properties.add(ColorProperty("shadowColor", shadowColor));
+    properties.add(DoubleProperty("elevation", elevation));
+    properties.add(ColorProperty("activeIndicatorColor", activeIndicatorColor));
+    properties.add(
+      DiagnosticsProperty<Size>("activeIndicatorSize", activeIndicatorSize),
+    );
+    properties.add(
+      DiagnosticsProperty<ShapeBorder>(
+        "activeIndicatorShape",
+        activeIndicatorShape,
+      ),
+    );
+    properties.add(
+      DiagnosticsProperty<WidgetStateProperty<IconThemeDataPartial>>(
+        "iconTheme",
+        iconTheme,
+      ),
+    );
+    properties.add(
+      DiagnosticsProperty<WidgetStateProperty<TextStyle>>(
+        "labelTextStyle",
+        labelTextStyle,
+      ),
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        other is NavigationDrawerThemeData &&
+            backgroundColor == other.backgroundColor &&
+            shadowColor == other.shadowColor &&
+            elevation == other.elevation &&
+            activeIndicatorColor == other.activeIndicatorColor &&
+            activeIndicatorSize == other.activeIndicatorSize &&
+            activeIndicatorShape == other.activeIndicatorShape &&
+            iconTheme == other.iconTheme &&
+            labelTextStyle == other.labelTextStyle;
+  }
+
+  @override
+  int get hashCode => Object.hash(
+    backgroundColor,
+    shadowColor,
+    elevation,
+    activeIndicatorColor,
+    activeIndicatorSize,
+    activeIndicatorShape,
+    iconTheme,
+    labelTextStyle,
+  );
+}
