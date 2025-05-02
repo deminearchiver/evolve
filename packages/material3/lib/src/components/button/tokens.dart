c588ba4ef3e02d893cb8b7a7d9a0c613f2722483
DeMineArchiver <minearchiver@gmail.com>
2025-05-02 20:16:30 +0300

    Remove material3 package

--- packages/material3/lib/src/components/button/tokens.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Token<T> {}
-
-class ColorToken extends Token<Color> {}
-
-class Tokens {}
--- packages/material3/lib/src/system/color.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-
-class ColorThemeDataPartial with Diagnosticable {}
-
-class ColorThemeData with Diagnosticable implements ColorThemeDataPartial {}
-
-class ColorTheme extends InheritedTheme {}
