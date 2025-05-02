82db49b2d50e8bf3b99ca3c6a3b68ec4aa932960
DeMineArchiver <minearchiver@gmail.com>
2025-05-02 21:34:47 +0300

    Add state theme

--- packages/material3_expressive/lib/material3_expressive.dart
+++ packages/material3_expressive/lib/material3_expressive.dart
@@ -2,7 +2,8 @@ library;
 
 export 'src/typeface/typeface.dart';
 
-export 'src/typescale/typescale.dart';
 export 'src/color/color.dart';
+export 'src/typescale/typescale.dart';
 export 'src/elevation/elevation.dart';
+export 'src/state/state.dart';
 export 'src/motion/motion.dart';
---
+++ packages/material3_expressive/lib/src/state/state.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+part 'state_theme_data_partial.dart';
+part 'state_theme_data.dart';
+part 'state_theme.dart';
