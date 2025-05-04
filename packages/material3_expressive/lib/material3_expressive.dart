06b8b6d2c9c472d33cc438f437ba24a1e4c2398d
DeMineArchiver <minearchiver@gmail.com>
2025-05-04 19:21:21 +0300

    Exported spring theme

--- packages/material3_expressive/lib/material3_expressive.dart
+++ packages/material3_expressive/lib/material3_expressive.dart
@@ -7,4 +7,7 @@ export 'src/styles/shape/shape.dart';
 export 'src/styles/typescale/typescale.dart';
 export 'src/styles/elevation/elevation.dart';
 export 'src/styles/state/state.dart';
+
+// TODO: completely refactor motion fater migrating to treelike theme structure
 export 'src/styles/motion/motion.dart';
+export 'src/styles/motion/spring.dart';
